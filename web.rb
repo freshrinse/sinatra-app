e12156b5946e661a0f7377f4bc196e3cc65c521e
trjohnst <trjohnst@ucsc.edu>
2013-01-31 01:42:30 -0800

    asset loading fix

--- web.rb
+++ web.rb
@@ -13,6 +13,6 @@ get '/quote' do
 	File.read(File.join('public', 'quote.html'))
 end
 
-get '/assets/img/logo.png' do
+get '/logo.png' do
 	File.read(File.join('public/assets/img', 'logo.png'))
 end
