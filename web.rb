598452a99eab5e869f71f1bf5a53bd7d2fa2c5d9
trjohnst <trjohnst@ucsc.edu>
2013-01-31 02:08:28 -0800

    rollback, weird bug about expecting a keyword

--- web.rb
+++ web.rb
@@ -17,19 +17,4 @@ get '/logo.png' do
 	File.read(File.join('public/assets/img', 'logo.png'))
 end
 
-get '/assets/css/style.css' do
-	File.read(File.join('public/assets/css', 'style.css'))
-end
-get '/assets/css/bootstrap.min.css' do
-	File.read(File.join('public/assets/css', 'bootstrap.min.css'))
-end
-get '/assets/css/bootstrap-responsive.min.css' do
-	File.read(File.join('public/assets/css', 'bootstrap-responsive.min.css'))
-end
 
-get '/assets/js/jquery.js' do
-	File.read(File.join('public/assets/js', 'jquery.js'))
-end
-get '/assets/js/bootstrap.plugins.js'
-	File.read(File.join('public/assets/js', 'boostrap.plugins.js'))
-end
