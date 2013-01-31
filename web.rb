a0dfa65d8db853ee24c5bdad6ca899c432e79180
trjohnst <trjohnst@ucsc.edu>
2013-01-31 01:29:08 -0800

    attempt at linking images

--- web.rb
+++ web.rb
@@ -13,6 +13,6 @@ get '/quote' do
 	File.read(File.join('public', 'quote.html'))
 end
 
-get 'assets/img/logo.png' do
-	File.read(File.join('public', '/assets/img/logo.png'))
+get '/logo.png' do
+	File.read(File.join('public', 'logo.png'))
 end
